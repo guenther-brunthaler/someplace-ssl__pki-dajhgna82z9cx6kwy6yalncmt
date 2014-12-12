49903a46e1c8373e5f4dfabeda584b84f5563141
Guenther Brunthaler <gb@xquad.emgenxx69lwyn5ctlr4nl64ul.local>
2014-12-12 13:10:00 +0100

    Consolidate all scripts into a single one

--- ca/generate_ca.sh
+++ ca/generate_ca.sh
@@ -1,2 +1,55 @@
 #! /bin/sh
-exec sh ../shared/generation_helper.sh ca "$1" --generate-self-signed
+SUFFIX=.pem
+sf=last_serial.txt
+
+set -e
+trap 'echo "Failure!" >& 2' 0
+
+kind=${0##*/}; kind=${kind%.sh}; kind=${kind#generate_}; test -n "$kind"
+PRVKEY_PREFIX=$kind-${1}${1:+-}private-key
+PUBCERT_PREFIX=$kind-${1}${1:+-}public-cert
+
+infile=$kind.info
+test -f "$infile"
+tpl=$infile.tmp
+
+if test -e "$sf"
+then
+	read sn < "$sf"
+	digits=`printf %s "$sn" | wc -c`
+	sn=${sn##0}
+else
+	sn=0
+	digits=5
+fi
+sn=`expr "$sn" + 1`
+sn=`printf '%0*u' "$digits" "$sn"`
+cpp -P -I../shared -DSERIAL_NUMBER=$sn "$infile" > "$tpl"
+
+pk=$PRVKEY_PREFIX-$sn$SUFFIX
+certtool --generate-privkey > "$pk"
+chmod 600 "$pk"
+pc=$PUBCERT_PREFIX-$sn$SUFFIX
+case $kind in
+	ca)
+		set -- --generate-self-signed
+		;;
+	*)
+		set -- \
+			--generate-certificate \
+			--load-ca-certificate ../ca/ca-public-cert.pem \
+			--load-ca-privkey ../ca/ca-private-key.pem
+esac
+certtool "$@" \
+	--load-privkey "$pk" \
+	--template "$tpl" \
+	--outfile "$pc" \
+2>& 1 | tee "$pc"_info.txt
+
+echo "$sn" > "$sf"
+rm -- "$tpl"
+
+ln -sf "$pk" "$PRVKEY_PREFIX$SUFFIX"
+ln -sf "$pc" "$PUBCERT_PREFIX$SUFFIX"
+
+trap - 0
--- client/generate_client.sh
+++ client/generate_client.sh
@@ -1,5 +1,1 @@
-#! /bin/sh
-exec sh ../shared/generation_helper.sh client "$1" \
-	--generate-certificate \
-	--load-ca-certificate ../ca/ca-public-cert.pem \
-	--load-ca-privkey ../ca/ca-private-key.pem
+../ca/generate_ca.sh
